cf86a158e6ca0c101adb1ddbf52d996bc99e26d8
Peter Collingridge <peter.collingridge@gmail.com>
2019-03-11 23:05:38 +0000

    Move colour into function

--- lesson-1/drawFunctions.js
+++ lesson-1/drawFunctions.js
@@ -2,9 +2,9 @@ var TAU = 2 * Math.PI;
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = 'rgb(40, 170, 10)';
 
 function drawBloop(bloop) {
+    ctx.fillStyle = 'rgb(40, 170, 10)';
     ctx.beginPath();
     ctx.arc(bloop.x, bloop.y, bloop.r, 0, TAU, true);
     ctx.fill();
